08b331bdabce5d1cadcede15e301800cd3d69aff
Szymon Miśkiewicz <szymonmiskiewicz@gmail.com>
2022-07-14 09:54:16 +0200

    Added condition

--- src/com/engeto/supper/Philosopher.java
+++ src/com/engeto/supper/Philosopher.java
@@ -19,7 +19,7 @@ public class Philosopher implements Runnable {
     @Override
     public void run() {
 
-        while(foodCounter< 50){
+        while(foodCounter< 10000){
             try {
                 doAction(System.nanoTime() + ": thinking");
                 synchronized (leftFork){
